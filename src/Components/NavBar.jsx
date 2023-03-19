a2c7fe41cbf3f6eab54124e1d5b64bc0f8b859e4
imat19 <vergara.reymart82@gmail.com>
2023-03-20 05:08:05 +0800

    added  links and buttons

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,35 +2,33 @@
 function NavBar() {
   return (
     <div className="">
-      <nav class="navbar navbar-expand-lg">
-        <div class="container-lg">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-lg">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <a class="navbar-brand" href="#"><img className="img-fluid" src="./images/blacklogo.png" alt="" width={50} /></a>
-          <div class="collapse navbar-collapse fw-bold" id="navbarTogglerDemo03">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 fs-5 text-center">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#home">Home</a>
+          <a className="navbar-brand" href="#home"><img className="img-fluid" src="./images/blacklogo.png" alt="" width={50} /></a>
+          <div className="collapse navbar-collapse fw-bold" id="navbarTogglerDemo03">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 fs-5 text-center">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#home">Home</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#aboutme">About</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#aboutme">About</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#projects">Projects</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#projects">Projects</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#contact">Contact</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#contact">Contact</a>
               </li>
             </ul>
             <div className="text-center">
-              <button className="btn  border border-2 border-dark p-2  fs-6 text-center"  target="_blank"><a className="text-decoration-none text-dark" href="./images/Raymart Vergara.pdf" target="_blank" >Download CV</a> </button>
+              <button className="btn  border border-2 border-dark p-2  fs-6 text-center" target="_blank"><a className="text-decoration-none text-dark" href="./images/Raymart Vergara.pdf" target="_blank" >Download CV</a> </button>
             </div>
           </div>
         </div>
-
       </nav>
-
     </div>
 
   );
